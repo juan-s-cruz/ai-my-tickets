bc77d936d1cffcf12ea6999996188b9b10377fcb
juan-s-cruz <jscruz19@gmail.com>
2025-09-22 18:45:52 +0200

    added router and basic views

--- ticket_system/ticketing_site/urls.py
+++ ticket_system/ticketing_site/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tickets.views import TicketViewSet
+
+router = DefaultRouter()
+router.register(r"tickets", TicketViewSet, basename="ticket")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
--- ticket_system/tickets/views.py
+++ ticket_system/tickets/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Ticket
+from .serializers import TicketSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [AllowAny]  # fully open for POC
