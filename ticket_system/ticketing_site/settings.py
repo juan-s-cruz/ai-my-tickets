ef68f40255cd16d0078b1f7eb183b0e8f099eb1f
juan-s-cruz <jscruz19@gmail.com>
2025-09-22 18:20:47 +0200

    added dotenv package to load environtment variables and remove hard-coding secrets

--- ticket_system/ticketing_site/settings.py
+++ ticket_system/ticketing_site/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g)w4c-b-!+wh*)z_!q-ak3a-#-h+%w0v6=vj5he2x@lz$pmifc"
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
