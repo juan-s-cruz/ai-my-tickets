63dfa431f8b7164407e4c7dc31a6b40cfe3bcf1b
juan-s-cruz <jscruz19@gmail.com>
2025-09-22 18:24:45 +0200

    added ticket model to models.py

--- ticket_system/tickets/models.py
+++ ticket_system/tickets/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class StatusChoices(models.TextChoices):
+    OPEN = "OPEN", "Open"
+    RESOLVED = "RESOLVED", "Resolved"
+    CLOSED = "CLOSED", "Closed"
+
+
+class Ticket(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    resolution_status = models.CharField(
+        max_length=8,
+        choices=StatusChoices.choices,
+        default=StatusChoices.OPEN,
+    )
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.resolution})"
