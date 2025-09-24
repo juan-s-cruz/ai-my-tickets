4be47004c4113a93870514102a853bd56794b20e
juan-s-cruz <jscruz19@gmail.com>
2025-09-24 18:26:21 +0200

    updated ticket_system settings to be accessible from ai_agent container

--- ticket_system/ticketing_site/settings.py
+++ ticket_system/ticketing_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "ticket-system", "ticket_system", "ai_agent"]
 
 
 # Application definition
